f249d45c86c523a79937716be6163fc3193ed27f
carlosyv <cyaltav@gmail.com>
2020-09-29 15:01:16 -0500

    Writing code makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,8 +4,6 @@
 ## Write a short comment describing this function
 #The first function, makeCacheMatrix creates a special "matrix" object that
 #can cache its inverse.
-
-makeCacheMatrix <- function(x = matrix()) {
 #1.  set the value of the matrix
 #2.  get the value of the matrix
 #3.  set the value of the inverse matrix
